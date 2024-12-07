7bf5f497e5bf2fd63599c285fc7b349c1da97531
onlytoseef <hellotoseef@gmail.com>
2024-12-07 21:43:32 +0500

    interview files

---
+++ test.js
@@ -0,0 +1,22 @@
+function one() {
+  const myName = "Toseef Rana";
+
+  function printName() {
+    const web = "youtube";
+    console.log(myName);
+  }
+  printName();
+}
+one();
+
+console.log(add(5));
+function add(num) {
+  return (num * (1 * 88)) / 4;
+}
+
+const myName = function () {
+  return console.log("Rana Toseef");
+};
+
+const reult = myName();
+console.log(reult);
