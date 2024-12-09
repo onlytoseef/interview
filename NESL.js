795f301499edc9f0ca87214d6433913b60bd6a72
onlytoseef <hellotoseef@gmail.com>
2024-12-09 20:47:33 +0500

    added interview questions

---
+++ NESL.js
@@ -0,0 +1,25 @@
+// Interbiew Questions
+// What is hoisting ?
+// Write a function that retrives a string
+// let a = [1,2,3,4,5] let b = a; b.push(6) let c = [...a] c.push(7) Output of a,b,c ?
+// What are promises
+// Question:let array1=[1,2,3,4,5,6,4,5,6,7], array2=[8,3,5,7,9,0,3,6] Find Count of unique common number in given arrays
+// how we differ from object from map
+// Hooks Use Memo useRef and useState
+// useMemo is used for ?
+// what is usecallback ?
+// What is react.lazy ?
+// What is high order components
+// How you can Implement search bar on react app when fetch data from server
+// Debounncing
+// brief about redux toolkit
+// What is node js and how it works ?
+// What are event loops ?
+//  What is indexing in mongodb
+// pagination in data query
+//  how to set refrencing in between two documents in mongo db
+// What are middleware ?
+// how you will set auth middleware ?
+// What is put and patch ?
+// what is callbackhell ?
+// write a function tahat takes an argument and return a promise if number >60 resolve, reject
