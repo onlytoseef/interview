0a16c9289a864826f15347436e496a816784c73e
onlytoseef <hellotoseef@gmail.com>
2024-12-12 19:55:11 +0500

    added another

--- more.js
+++ more.js
@@ -16,3 +16,21 @@ Promise.resolve().then(console.log("D"));
 // we can also run aysnchoronous task by using call back and aysn await
 // when a task is synchornous it is set in the queue list of browser (callback queue)
 // then after its time completed it is moved to call stack with the help of event loop
+// here is  another example
+
+async function toseef() {
+  return 10;
+}
+
+console.log("A");
+console.log("B");
+setTimeout(() => {
+  console.log("C");
+}, 0);
+toseef().then(() => console.log("Function"));
+console.log("D");
+
+// Here is one asynchoronous function with promise
+// there is another queue for promises called micro task queue
+// call stack prefer task queue
+// so call stak will prefer promises over settimeout
